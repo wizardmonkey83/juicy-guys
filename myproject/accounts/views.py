0f5bc99bc5300726be8ada360ee7c865bb52ab3c
Benjamin <benjamin630@icloud.com>
2025-10-27 12:45:21 -0700

    created reciever to handle profile creation

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django.contrib import messages
 from django.db.models import Q
@@ -26,13 +28,18 @@ def signup_view(request):
                 messages.error(request, "Username and/or Email already exists")
 
             user = form.save()
-            Profile.objects.create(user=user)
             login(request, user)
             return redirect("user_home")
         
     else:
         form = SignUpForm()
     return render(request, "accounts/signup.html", {"form": form})
+
+# so that user profiles will be created if they login through github
+@receiver(post_save, sender=User)
+def user_created(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 def login_view(request):
     if request.method == "POST":
