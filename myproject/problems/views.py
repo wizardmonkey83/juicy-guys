25b562caf33d6b42a8550335d4ba465fdcaa8d9b
Benjamin <benjamin630@icloud.com>
2025-11-04 15:19:57 -0800

    troubleshooting error with batch submission

--- myproject/problems/views.py
+++ myproject/problems/views.py
@@ -290,6 +290,16 @@ def process_submit_code(request):
                         testcase_id_map[i] = testcase.id
 
                     batch_payload = {"submissions": submissions_payload}
+
+                    print("--- START: JUDGE0 BATCH PAYLOAD ---")
+                    try:
+                        # This will pretty-print the JSON data
+                        print(json.dumps(batch_payload, indent=2))
+                    except Exception as e:
+                        print(f"Could not dump JSON: {e}")
+                    print("--- END: JUDGE0 BATCH PAYLOAD ---")
+                    
+                    return render(request, "problems/page/submitting.html")
                     
                     response = requests.post("http://159.203.137.178:2358/submissions/batch?wait=false", json=batch_payload)
                     response.raise_for_status() 
@@ -309,15 +319,7 @@ def process_submit_code(request):
                     request.session["language_id"] = language_id
                     request.session["code"] = source_code
 
-                    print("--- START: JUDGE0 BATCH PAYLOAD ---")
-                    try:
-                        # This will pretty-print the JSON data
-                        print(json.dumps(batch_payload, indent=2))
-                    except Exception as e:
-                        print(f"Could not dump JSON: {e}")
-                    print("--- END: JUDGE0 BATCH PAYLOAD ---")
                     
-                    return render(request, "problems/page/submitting.html")
 
                 except requests.exceptions.RequestException as e:
                     # to troubleshoot, pass e into the f string, for now leaving it as this. 
